24df37b1afdc322e2054a958cea10c101d80c730
nourbakleh1 <nourbakleh1@gmail.com>
2024-07-12 23:14:33 +0300

    update hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,8 +5,8 @@ import React from 'react';
 
 const Hero = () => {
   return (
-<div className='bg-black_color wow  rotateInDownLeft' >
-<div data-wow-delay={".4s"} className="wow  rotateInDownLeft h-[300px] sm:h-[350px] md:h-[400px] xl:h-[500px] 2xl:h-[600px]">
+<div className='bg-black_color' >
+<div  className="wow  bounceIn h-[300px] sm:h-[350px] md:h-[400px] xl:h-[500px] 2xl:h-[600px]">
 
       <Carousel pauseOnHover slideInterval={5000}>
         <img className='bg-cover object-cover h-full' src="/assets/images/Hero_images/hero-1.jpg" alt="..." />
