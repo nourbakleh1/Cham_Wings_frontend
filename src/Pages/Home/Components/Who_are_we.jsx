8651a0592e5f7b11fbebb01a9ba4f80ebb3e0144
nourbakleh1 <nourbakleh1@gmail.com>
2024-07-19 17:21:02 +0300

    update home page

--- src/Pages/Home/Components/Who_are_we.jsx
+++ src/Pages/Home/Components/Who_are_we.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Headings from '../../../Components/Headings/Headings';
+import "./Responsibilty.css"
 
 const Who_are_we = () => {
   return (
-    <section className='bg-off_white flex justify-between items-center gap-10 p-1 md:p-5 flex-wrap md:flex-nowrap'>
-    <div  className=' md:grow-[1] pt-[50px] px-0 md:px-[30px] text-center basis-[100%] md:basis-[400px]'>
+    <section className='bg-off_white py-[20px] lg:py-[100px] flex justify-between items-center gap-10 p-1 flex-wrap md:flex-nowrap'>
+    <div  className=' md:grow-[1] px-0 md:px-[30px] text-center basis-[100%] md:basis-[400px]'>
     <Headings element={"h1"} color='#e8e8e8'>who are we</Headings>
-    <div className='p-5'>
+    <div>
     <Headings element={"p"} color_P='#000'>
      Cham Wings Airlines, the first Syrian private airline company
      was established at the end of 2007 with a national capital, as one of the
@@ -20,14 +21,8 @@ const Who_are_we = () => {
      <Headings element={"h2"} color='#00529B'>Fly Beyond The Limits</Headings>
      <Headings element={"p"} color_P='#000'>16 Years in The Sky Flying Together</Headings>
     </div>
-    <div  className='md:grow-[1] basis-[100%] text-center  md:basis-[300px] bg-secoundary_color_1 p-2 rounded-lg hover:bg-primary_color_1 hover:p-3 transition-all delay-150'>
-      <Headings element={"p"} color_P='#fff'>Cham Wings Airline</Headings>
-     <video className='xl:h-[370px]' width="100%" controls>
-     <source src="/assets/videos/Let_s_fly_together!(360p).mp4" type="video/mp4"/>
-     <source src="movie.ogg" type="video/ogg"/>
-     Your browser does not support the video tag.
-     </video>
-     
+    <div  className='md:grow-[1/2] 2xl:grow-[1/3] m-auto text-center move-up   flex justify-center items-center  '>
+      <img className='object-cover h-[200px] sm:h-[300px] w-[200px] sm:w-[350px] 2xl:w-[450px] ' src='/assets/images/pngwing.com.webp'/>
     </div>
     </section>
   )
