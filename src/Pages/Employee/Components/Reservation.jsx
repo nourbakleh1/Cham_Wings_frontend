67f9b6251a92f31fff1815377c5c64bfbd06ecf5
nourbakleh1 <nourbakleh1@gmail.com>
2024-07-24 23:38:03 +0300

    update chatbot && update public request

--- src/Pages/Employee/Components/Reservation.jsx
+++ src/Pages/Employee/Components/Reservation.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import Chatbot from './Chatbot'
 
 const Reservation = () => {
     useEffect(()=>{
@@ -8,7 +7,6 @@ const Reservation = () => {
   return (
     <div>
     <div className='h-[1500px] bg-gradient-to-t bg-gray_color'>
-    <Chatbot/>
     </div>
     </div>
   )
