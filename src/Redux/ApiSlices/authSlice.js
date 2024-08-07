ade1fa39cefc74157ab8bc88aecbfa1e81543365
nourbakleh1 <nourbakleh1@gmail.com>
2024-08-07 14:12:37 +0300

    update authSlice

--- src/Redux/ApiSlices/authSlice.js
+++ src/Redux/ApiSlices/authSlice.js
@@ -6,33 +6,34 @@ export const register=createAsyncThunk("auth/register",async(data,ThunkApi)=>{
     const {rejectWithValue}=ThunkApi;
 
     try{
-        const {data}=await publicRequest.post("/api/register",data);
-        return data
+        const res=await publicRequest.post("/api/register",data);
+        return res.data
     }
     catch(error){
-        return rejectWithValue(error.message)
+        return rejectWithValue(error)
     }
 });
 export const checkVerify=createAsyncThunk("auth/checkVerify",async(data,ThunkApi)=>{
     const {rejectWithValue}=ThunkApi;
+    
 
     try{
-        const {data}=await publicRequest.post(`/api/register-verify/${data}`,data);
-        return data
+        const res=await publicRequest.post(`/api/register-verify/${data.email}`,data.code);
+        return res.data
     }
     catch(error){
-        return rejectWithValue(error.message)
+        return rejectWithValue(error)
     }
 });
 export const login=createAsyncThunk("auth/login",async(data,ThunkApi)=>{
     const {rejectWithValue}=ThunkApi;
 
     try{
-        const {data}=await publicRequest.post(`/api/login`,data);
-        return data.data
+        const res=await publicRequest.post(`/api/login`,data);
+        return res.data
     }
     catch(error){
-        return rejectWithValue(error.message)
+        return rejectWithValue(error)
     }
 });
 
