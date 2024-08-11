7a56c2cfa6af8f01afdf24984ccbdc0d9bea1450
nourbakleh1 <nourbakleh1@gmail.com>
2024-08-11 10:04:07 +0300

    update login page

--- src/Pages/Forms/Login/Login.jsx
+++ src/Pages/Forms/Login/Login.jsx
@@ -21,16 +21,16 @@ const Login = () => {
       const role = res?.data?.user?.employee?.roles[0]?.role_id;
       if(res?.data){
         if(role == 4){
-            localStorage.setItem("token",JSON.stringify(res?.data?.token?.split("|")[1]))
+            localStorage.setItem("token",JSON.stringify(res?.data?.token))
             navigate("/admin_dashboard",{replace:true})
           }
           else if(role == 5 || role == 6 || role == 7 || role == 8 ||role == 9 || role == 10 ||role == 11 || role == 12 ||role == 13 || role == 14)
           {
-            localStorage.setItem("token",JSON.stringify(res?.data?.token?.split("|")[1]))
+            localStorage.setItem("token",JSON.stringify(res?.data?.token))
             navigate("/dashboard/employee",{replace:true})
           }
           else{
-            localStorage.setItem("token",JSON.stringify(res?.data?.token?.split("|")[1]))
+            localStorage.setItem("token",JSON.stringify(res?.data?.token))
             navigate("/",{replace:true})
           }
        }
