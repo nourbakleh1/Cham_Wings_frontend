54500769f0ee690677f75f2c0700b86032f5d487
nourbakleh1 <nourbakleh1@gmail.com>
2024-09-01 02:51:02 +0300

    update Pagnation && add react_paginate

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Pagination.css"
+import ReactPaginate from 'react-paginate';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBackward, faForward } from '@fortawesome/free-solid-svg-icons';
 
 const Pagination = ({page,setPage,totalElement,perPage=15}) => {
-  const numer= Math.ceil(totalElement / perPage);
-  const newArr=[];
-  for(let i=1;i<=numer;i++){
-    newArr.push(i)
+  const number= Math.ceil(totalElement / perPage);
+
+  
+ 
+  const handelPage=({selected})=>{
+    
+    setPage(selected+1)
   }
   return (
     <>
-      <nav aria-label="Page navigation example" className='w-full items-center flex p-5'>
-  <ul className="inline-flex -space-x-px text-base h-8 border-0 outline-none ">
+      <nav aria-label="Page navigation example" className='w-full items-center  flex p-5 scrollll'>
+  {/* <ul className="inline-flex -space-x-px text-base h-8 border-0 outline-none ">
     <li>
       <button  onClick={()=>setPage(page - 1)} disabled= {page == 1} className= {`flex items-center justify-center px-2 h-8 shadow-xl shadow-black_color/40 ms-0 disabled:cursor-not-allowed leading-tight text-secoundary_color_1/80 bg-black/20  rounded-s-lg hover:bg-primary_color_1 hover:text-white_color `}>Prev</button>
     </li>
@@ -26,7 +32,21 @@ const Pagination = ({page,setPage,totalElement,perPage=15}) => {
     <li>
       <button onClick={()=>setPage(page + 1)} disabled={newArr.length == page} className="flex items-center justify-center px-2 h-8 disabled:cursor-not-allowed shadow-xl shadow-black_color/40 leading-tight text-secoundary_color_1/80 bg-black/20  rounded-e-lg hover:bg-primary_color_1 hover:text-white_color  ">Next</button>
     </li>
-  </ul>
+  </ul> */}
+  <ReactPaginate
+    breakLabel="*****"
+    containerClassName={"conainerPAGE"}
+    nextLabel={<FontAwesomeIcon icon={faForward} className='text-[20px]'/>}
+    previousLabel={<FontAwesomeIcon icon={faBackward} className='text-[20px]'/>}
+    pageCount={number}
+    onPageChange={handelPage}
+    activeClassName={"pageActive"}
+    previousClassName={"pagePrev"}
+    nextLinkClassName={"pageNext"}
+    disabledClassName	={"disablePage"}
+    pageLinkClassName={"pageclass"}
+    initialPage={page}
+  />
 </nav>
     </>
   )
