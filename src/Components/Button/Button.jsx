a7f4a3bd16df5ded7ddae0ec76feaeb5e8144fe3
Bashar Usif <bashar.usif@smartsoft.sy>
2024-09-04 00:06:54 +0300

    add width attribute

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./Button.css";
 
-const Button = ({children,color,onClick,padding="10px",text="#fff"},props) => {
+const Button = ({children,color,onClick,padding="10px",text="#fff",width="auto"},props) => {
   return (
-    <button onClick={onClick} style={{backgroundColor : color,padding:padding,color:text}} className="btn-style">{children}</button>
+    <button onClick={onClick} style={{backgroundColor : color,padding:padding,color:text,width:width}} className="btn-style">{children}</button>
   )
 }
 
