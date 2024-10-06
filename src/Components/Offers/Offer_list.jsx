13fcc8d2c8d6e555bc0debc1b6cfa3894b9c8b29
nourbakleh1 <nourbakleh1@gmail.com>
2024-10-06 11:35:32 +0300

    update offerlist

--- src/Components/Offers/Offer_list.jsx
+++ src/Components/Offers/Offer_list.jsx
@@ -6,27 +6,28 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getOffersUser } from '../../Redux/ApiSlices/authSlice';
 import Loading2 from '../Loading/Loading2';
 import { useNavigate } from 'react-router-dom';
+import useGetOffer from '../../Hooks/useGetOffer';
 
 const Offer_list = () => {
-  const dispatch=useDispatch();
   const navigate = useNavigate();
-    const { user_offers,error,isLoading}=useSelector((state)=>state.auth);
  
    // while refresh page
-   useEffect(()=>{
-    dispatch(getOffersUser(1)).unwrap().then((res)=>{
+  //  useEffect(()=>{
+  //   dispatch(getOffersUser(1)).unwrap().then((res)=>{
        
-    }).catch((rej)=>{
-        return toast.error(rej?.response?.data?.message);
-    })
-  },[]);
+  //   }).catch((rej)=>{
+  //       return toast.error(rej?.response?.data?.message);
+  //   })
+  // },[]);
+  const{isLoading,data:user_offers} = useGetOffer();
+    console.log(user_offers)
   return (
     
     <>
     <div className='mt-[100px]'>
         <Headings element={"h1"}>Offers</Headings>
         <div className='flex justify-center gap-12  items-center flex-wrap p-5'>
-         {isLoading ? <Loading2/>: user_offers?.data?.data?.slice(0,6)?.map((offer)=>{
+         {isLoading ? <Loading2/>: user_offers?.data?.data?.data?.slice(0,6)?.map((offer)=>{
             return <Offer_item key={offer.offer_id} offer={offer}/>
         })}
         </div>
