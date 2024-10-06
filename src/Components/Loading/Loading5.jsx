8b3f0d5554b37ad3edd13cef1ebebdf03ef82892
nourbakleh1 <nourbakleh1@gmail.com>
2024-10-06 11:35:15 +0300

    add new loading

---
+++ src/Components/Loading/Loading5.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./load.css"
+
+const Loading5 = () => {
+  return (
+    <div className=' h-[80vh] w-full mt-[75px] flex justify-center items-center bg-black/5'>
+        <div className='dotted'></div>
+    </div>
+  )
+}
+
+export default Loading5
